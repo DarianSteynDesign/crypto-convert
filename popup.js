f8149719225328b390fc458d5101db52f14ab5ea
DarianSteynDesign <dariansteyn.design@gmail.com>
2020-08-03 18:07:20 +0200

    Added select functionality on list and loaded coin images

--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
 //SUCCESS METHODS
 
 var successMethods = (function(){
-    var getTop100ListSuccess = function(data) {
-        let top100List = data;
 
-        uiActions.loadCurrencyList($("#cryptoSymbolList"), data);
+    var getTop100ListSuccess = function(data) {
+        uiActions.getTop100Data(data);
+
+        uiActions.loadCurrencyList($("#cryptoSymbolList"));
         uiActions.toggleElementState($("#toggleLbl"));
     }
 
@@ -82,6 +83,13 @@ var eventListeners = (function(){
         uiActions.toggleElementState($("#coinCont"));
     });
 
+    $("body").on("click", ".crypto-list-item", function(event){
+        let selectedId = $(this).data('id');
+        let name = $(this).data('name');
+        let fullname = $(this).data('fullname');
+        uiActions.loadSelectedItem(selectedId);
+    });
+
     return {
         
     }
@@ -90,6 +98,12 @@ var eventListeners = (function(){
 //UI ACTIONS
 
 var uiActions = (function(){
+    var top100 = [];
+
+    //Get Top 100 List
+    var getTop100Data = function(response){
+        top100 = response.Data;
+    }
 
     //Toggle Loading
     var toggleElementState = function(elementToggle) {
@@ -98,19 +112,41 @@ var uiActions = (function(){
     }
 
     //Populate currencyList
-    var loadCurrencyList = function(listElement, response) {
+    var loadCurrencyList = function(listElement) {
         let coinList = "";
-        console.log(listElement, response);
+        console.log(listElement, top100);
         
-        response.Data.forEach(function(item){
-            coinList += "<li class='cyrpto-list-item'>" + item.CoinInfo.Name + "</li>";
+        top100.forEach(function(item){
+            coinList += "<li class='crypto-list-item' data-id='" + item.CoinInfo.Id + 
+                        "' data-name='" + item.CoinInfo.Name + 
+                        "' data-fullname='" + item.CoinInfo.FullName + 
+                        "' > <img class='crypto-item-img' src='https://www.cryptocompare.com/" + item.CoinInfo.ImageUrl + 
+                        " ' /> <p class='crypto-item-text'>" + item.CoinInfo.Name + "</p></li>";
         });
         
         listElement.html(coinList);
     }
 
+    var loadSelectedItem = function(selectedId) {
+        selectedId = JSON.stringify(selectedId);
+        top100.forEach(function(item){
+            let topPrice = Object.values(item.RAW)[0].PRICE;
+            let coinName = item.CoinInfo.Name;
+
+            (item.CoinInfo["Id"] == selectedId) ? updateUiWithSelected(topPrice, coinName) : '' ;
+        });
+
+        function updateUiWithSelected(selectedPrice, selectedName){
+            $("#sym1").val(selectedPrice);
+            $("#priceSym1").text(selectedName);
+            toggleElementState($("#coinCont"));
+        }
+    }
+
     return {
+        getTop100Data,
         toggleElementState,
-        loadCurrencyList
+        loadCurrencyList,
+        loadSelectedItem
     }
 })(uiActions);
